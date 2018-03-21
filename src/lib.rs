0c2bc21624b8b3feda9f4133df25f454e9ad9dd2
Paul Osborne <osbpau@gmail.com>
2018-03-21 01:38:29 -0500

    Add license information (MIT / Apache-2.0)
    Signed-off-by: Paul Osborne <osbpau@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Copyright (c) 2018 The rust-gpio-cdev Project Developers.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
