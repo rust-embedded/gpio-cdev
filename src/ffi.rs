b964056ab3e0afe03a81f209478e2034ec72a126
Paul Osborne <osbpau@gmail.com>
2018-03-22 00:08:30 -0500

    WIP: Hacking together first pass based on gpiod C++ iface
    Signed-off-by: Paul Osborne <osbpau@gmail.com>

---
+++ src/ffi.rs
@@ -0,0 +1,90 @@
+// Copyright (c) 2018 The rust-gpio-CDC Project Developers.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use libc;
+
+const GPIOHANDLES_MAX: usize = 64;
+
+// struct gpiochip_info
+#[repr(C)]
+pub struct gpiochip_info {
+    pub name: [libc::c_char; 32],
+    pub label: [libc::c_char; 32],
+    pub lines: libc::uint32_t,
+}
+
+#[repr(C)]
+pub struct gpioline_info {
+    pub line_offset: libc::uint32_t,
+    pub flags: libc::uint32_t,
+    pub name: [libc::c_char; 32],
+    pub consumer: [libc::c_char; 32],
+}
+
+// Line Request Flags
+bitflags! {
+    struct GpioHandleRequest: libc::uint32_t {
+        const INPUT = (1 << 0);
+        const OUTPUT = (1 << 1);
+        const ACTIVE_LOW = (1 << 2);
+        const OPEN_DRAIN = (1 << 3);
+        const OPEN_SOURCE = (1 << 4);
+    }
+}
+
+#[repr(C)]
+struct gpiohandle_request {
+    lineoffsets: [libc::uint32_t; GPIOHANDLES_MAX],
+    flags: libc::uint32_t,
+    default_values: [libc::uint8_t; GPIOHANDLES_MAX],
+    consumer_label: [libc::c_char; 32],
+    lines: libc::uint32_t,
+    fd: libc::c_int,
+}
+
+#[repr(C)]
+struct gpiohandle_data {
+    values: [libc::uint8_t; GPIOHANDLES_MAX],
+}
+
+bitflags! {
+    pub struct GpioEventRequestFlags: libc::uint32_t {
+        const RISING_EDGE = (1 << 0);
+        const FALLING_EDGE = (1 << 1);
+        const BOTH_EDGES = Self::RISING_EDGE.bits | Self::FALLING_EDGE.bits;
+    }
+}
+
+#[repr(C)]
+pub struct gpioevent_request {
+    lineoffset: libc::uint32_t,
+    handleflags: libc::uint32_t,
+    eventflags: libc::uint32_t,
+    consumer_label: [libc::c_char; 32],
+    fd: libc::c_int,
+}
+
+#[repr(C)]
+pub enum GpioEventType {
+    RisingEdge = 0x01,
+    FallingEdge = 0x02,
+}
+
+#[repr(C)]
+pub struct gpioevent_data {
+    timestamp: libc::uint64_t,
+    id: libc::uint32_t,
+}
+
+ioctl!(read gpio_get_chipinfo_ioctl with 0xB4, 0x01; gpiochip_info);
+ioctl!(readwrite gpio_get_lineinfo_ioctl with 0xB4, 0x02; gpioline_info);
+ioctl!(readwrite gpio_get_linehandle_ioctl with 0xB4, 0x03; gpiohandle_request);
+ioctl!(readwrite gpio_get_lineevent_ioctl with 0xB4, 0x04; gpioevent_request);
+
+ioctl!(readwrite gpiohandle_get_line_values_ioctl with 0xB4, 0x08; gpiohandle_data);
+ioctl!(readwrite gpiohandle_set_line_values_ioctl with 0xB4, 0x09; gpiohandle_data);
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,157 @@
-// Copyright (c) 2018 The rust-gpio-cdev Project Developers.
+// Copyright (c) 2018 The rust-gpio-CDC Project Developers.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+
+// `error_chain!` can recurse deeply
+#![recursion_limit = "1024"]
+
+#[macro_use]
+extern crate bitflags;
+#[macro_use]
+extern crate error_chain;
+extern crate libc;
+#[macro_use]
+extern crate nix;
+
+use std::sync::Arc;
+use std::ffi::CStr;
+use std::fs::File;
+use std::mem;
+use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::path::Path;
+
+mod ffi;
+
+mod errors {
+    error_chain! {
+        types {
+            Error,
+            ErrorKind,
+            ResultExt,
+            Result;
+        }
+
+        foreign_links {
+            Nix(::nix::Error);
+            Io(::std::io::Error);
+
+        }
+    }
+}
+
+pub use errors::*;
+
+// Informational Flags
+bitflags! {
+    pub struct LineFlags: libc::uint32_t {
+        const KERNEL = (1 << 0);
+        const IS_OUT = (1 << 1);
+        const ACTIVE_LOW = (1 << 2);
+        const OPEN_DRAIN = (1 << 3);
+        const OPEN_SOURCE = (1 << 4);
+    }
+}
+
+#[derive(Debug)]
+struct InnerChip {
+    pub file: File,
+    pub name: String,
+    pub label: String,
+    pub lines: u32,
+}
+
+#[derive(Debug)]
+pub struct Chip {
+    inner: Arc<Box<InnerChip>>
+}
+
+impl Chip {
+
+    /// Open the GPIO Chip at the provided path (/dev/gpiochip<N>)
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Chip> {
+        let mut f = File::open(path)?;
+        let info: ffi::gpiochip_info = unsafe { mem::uninitialized() };
+        let _ = unsafe { ffi::gpio_get_chipinfo_ioctl(f.as_raw_fd(), &mut info)? };
+
+        Ok(Chip {
+            inner: Arc::new(Box::new(InnerChip {
+                file: f,
+                name: CStr::from_ptr(info.name.as_ptr()).to_string_lossy().into_owned(),
+                label: CStr::from_ptr(info.label.as_ptr()).to_string_lossy().into_owned(),
+                lines: info.lines,
+            }))
+        })
+    }
+
+    pub fn name(&self) -> &str {
+        self.inner.name.as_str()
+    }
+
+    pub fn label(&self) -> &str {
+        self.inner.label.as_str()
+    }
+
+    pub fn num_lines(&self) -> u32 {
+        self.inner.lines
+    }
+
+    pub fn get_line(&mut self, offset: u32) -> Result<Line> {
+        let mut line_info = ffi::gpioline_info {
+            line_offset: offset,
+            flags: 0,
+            name: mem::zeroed(),
+            consumer: mem::zeroed(),
+        };
+        let _ = unsafe { ffi::gpio_get_lineinfo_ioctl(self.inner.file.as_raw_fd(), &mut line_info)? };
+
+        Ok(Line {
+            chip: self.inner.clone(),
+            offset: offset,
+            flags: LineFlags::from_bits_truncate(line_info.flags),
+            name: CStr::from_ptr(line_info.name.as_ptr()).to_string_lossy().into_owned(),
+            consumer: CStr::from_ptr(line_info.consumer.as_ptr()).to_string_lossy().into_owned(),
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Line {
+    chip: Arc<Box<InnerChip>>,
+    offset: u32,
+    flags: LineFlags,
+    name: String,
+    consumer: String,
+}
+
+impl Line {
+    pub fn offset(&self) -> u32 {
+        self.offset
+    }
+
+    pub fn flags(&self) -> LineFlags {
+        self.flags
+    }
+
+    pub fn name(&self) -> &str {
+        self.name.as_str()
+    }
+
+    pub fn consumer(&self) -> &str {
+        self.consumer.as_str()
+    }
+
+    pub fn chip(&self) -> Chip {
+        Chip {
+            inner: self.chip.clone()
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct LineHandle {
+    file: File,
+}
