30286b5003bed02afe123dc1f75aeb4d0d138aab
Paul Osborne <osbpau@gmail.com>
2018-09-23 01:43:05 -0500

    Add travisci build support
    Signed-off-by: Paul Osborne <osbpau@gmail.com>

---
+++ ci/script.sh
@@ -0,0 +1,24 @@
+# This script takes care of testing your crate
+
+set -ex
+
+main() {
+    cross build --target $TARGET
+    cross build --target $TARGET --release
+
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+
+    cross test --target $TARGET
+    cross test --target $TARGET --release
+
+    # No main binary, so skip the 'cross run' portion
+    # cross run --target $TARGET
+    # cross run --target $TARGET --release
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
